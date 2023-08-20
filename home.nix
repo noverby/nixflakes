b4c9f611b2d32d5b7037277f3d43de377f8dbe71
Niclas Overby <niclas@overby.me>
2023-08-20 16:06:48 +0200

    pkgs: add protonmail-bridge

--- home.nix
+++ home.nix
@@ -13,6 +13,7 @@
     distrobox
     lldb
     util-linux
+    protonmail-bridge
 
     fd
     glab
@@ -794,24 +795,30 @@ in
       };
     };
 
-    systemd = {
-      user = {
-        services = {
-          tmux = {
-            Unit = {
-              Description = "Start tmux in detached session";
-            };
-
-            Service = {
-              Type = "forking";
-              ExecStart = "${pkgs.tmux}/bin/tmux new-session -s %u -d";
-              ExecStop = "${pkgs.tmux}/bin/tmux kill-session -t %u";
-            };
-
-            Install = {
-              WantedBy = ["default.target"];
-            };
-          };
+    systemd.user.services = {
+      tmux = {
+        Unit = {
+          Description = "Start tmux in detached session";
+        };
+        Service = {
+          Type = "forking";
+          ExecStart = "${pkgs.tmux}/bin/tmux new-session -s %u -d";
+          ExecStop = "${pkgs.tmux}/bin/tmux kill-session -t %u";
+        };
+        Install = {
+          WantedBy = ["default.target"];
+        };
+      };
+      protonmail-bridge = {
+        Unit = {
+          Description = "Service to run the Protonmail bridge client";
+          After = "network.target";
+        };
+        Service = {
+          ExecStart = "${pkgs.protonmail-bridge}/bin/protonmail-bridge --noninteractive";
+        };
+        Install = {
+          WantedBy = ["default.target"];
         };
       };
     };
