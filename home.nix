b5f0b63ef57db034900de4de612e6408a5498e85
Niclas Overby <niclas@overby.me>
2023-08-20 10:19:03 +0200

    programs: add difftastic

--- home.nix
+++ home.nix
@@ -349,6 +349,61 @@ in
         '';
       };
 
+      git = {
+        enable = true;
+        userName = "Niclas Overby";
+        userEmail = "niclas@overby.me";
+        lfs = {
+          enable = true;
+        };
+        difftastic = {
+          enable = true;
+        };
+        extraConfig = {
+          core = {
+            autocrlf = "input";
+            editor = "vi --wait";
+          };
+          push = {
+            default = "simple";
+            autoSetupRemote = true;
+          };
+          pull = {
+            rebase = true;
+          };
+          merge = {
+            tool = "vi";
+          };
+          mergetool = {
+            vi = {
+              cmd = "vi --wait $MERGED";
+            };
+          };
+          diff = {
+            tool = "vi";
+          };
+          difftool = {
+            vi = {
+              cmd = "vi --wait --diff $LOCAL $REMOTE";
+            };
+          };
+          color = {
+            ui = "auto";
+          };
+          credential = {
+            helper = "store";
+          };
+          filter = {
+            lfs = {
+              clean = "git-lfs clean -- %f";
+              smudge = "git-lfs smudge -- %f";
+              process = "git-lfs filter-process";
+              required = true;
+            };
+          };
+        };
+      };
+
       vscode = {
         enable = true;
         package = pkgs.vscodium;
@@ -658,58 +713,6 @@ in
             "when" = "inFileBrowser";
           }
         ];
-      };
-
-      git = {
-        enable = true;
-        userName = "Niclas Overby";
-        userEmail = "niclas@overby.me";
-        lfs = {
-          enable = true;
-        };
-        extraConfig = {
-          core = {
-            autocrlf = "input";
-            editor = "vi --wait";
-          };
-          push = {
-            default = "simple";
-            autoSetupRemote = true;
-          };
-          pull = {
-            rebase = true;
-          };
-          merge = {
-            tool = "vi";
-          };
-          mergetool = {
-            vi = {
-              cmd = "vi --wait $MERGED";
-            };
-          };
-          diff = {
-            tool = "vi";
-          };
-          difftool = {
-            vi = {
-              cmd = "vi --wait --diff $LOCAL $REMOTE";
-            };
-          };
-          color = {
-            ui = "auto";
-          };
-          credential = {
-            helper = "store";
-          };
-          filter = {
-            lfs = {
-              clean = "git-lfs clean -- %f";
-              smudge = "git-lfs smudge -- %f";
-              process = "git-lfs filter-process";
-              required = true;
-            };
-          };
-        };
       };
 
       tmux = {
