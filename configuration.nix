e92c4908b34eef8c1f19f9c25bfed0aadc4814db
Niclas Overby <niclas@overby.me>
2023-08-23 16:35:54 +0200

    nixos: add initial config

---
+++ configuration.nix
@@ -0,0 +1,77 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    <home-manager/nixos>
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Network
+  networking.hostName = "levitas";
+  networking.networkmanager.enable = true;
+
+  # Locale
+  console.keyMap = "dk-latin1";
+  time.timeZone = "Europe/Copenhagen";
+  i18n.defaultLocale = "en_DK.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "da_DK.UTF-8";
+    LC_IDENTIFICATION = "da_DK.UTF-8";
+    LC_MEASUREMENT = "da_DK.UTF-8";
+    LC_MONETARY = "da_DK.UTF-8";
+    LC_NAME = "da_DK.UTF-8";
+    LC_NUMERIC = "da_DK.UTF-8";
+    LC_PAPER = "da_DK.UTF-8";
+    LC_TELEPHONE = "da_DK.UTF-8";
+    LC_TIME = "da_DK.UTF-8";
+  };
+
+  # X11
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    layout = "dk";
+    xkbVariant = "";
+  };
+
+  # Audio
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+
+  # Users
+  users.users.noverby = {
+    isNormalUser = true;
+    description = "Niclas Overby";
+    extraGroups = ["networkmanager" "wheel"];
+  };
+  home-manager.users.noverby = import ./home.nix;
+
+  # Packages
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
+
+  # Services
+  services = {
+    printing.enable = true;
+    openssh.enable = true;
+    flatpak.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+  };
+
+  system.stateVersion = "23.05";
+}
