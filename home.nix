584e956dc0b20b4247c7875f29d3264b56eaaf94
Niclas Overby <niclas@overby.me>
2023-08-23 17:02:30 +0200

    dconf: move to separate nix file

--- home.nix
+++ home.nix
@@ -809,43 +809,5 @@ in
       };
     };
 
-    dconf.settings = {
-      "org/gnome/shell" = {
-        enabled-extensions = map (extension: extension.extensionUuid) gnomeExtensions;
-        disabled-extensions = [];
-        favorite-apps = [
-          "firefox.desktop"
-          "codium.desktop"
-          "org.gnome.Console.desktop"
-          "org.gnome.Nautilus.desktop'"
-          "org.gnome.Geary.desktop"
-        ];
-      };
-      "org/gnome/desktop/peripherals/mouse" = {
-        natural-scroll = true;
-      };
-      "org/gnome/desktop/peripherals/touchpad" = {
-        natural-scroll = true;
-        tap-to-click = true;
-      };
-      "org/gnome/desktop/calendar" = {
-        show-weekdate = true;
-      };
-      "org/gnome/desktop/interface" = {
-        clock-show-date = true;
-      };
-      "org/gnome/settings-daemon.plugins/color" = {
-        night-light-enabled = true;
-      };
-      "com/github/amezin/ddterm" = {
-        command = "custom-command";
-        custom-command = "tmux-dir";
-        ddterm-toggle-hotkey = [
-          "onehalf"
-        ];
-      };
-      "org/gnome/shell/extensions/auto-move-windows" = {
-        application-list = ["com.spotify.Client.desktop:2"];
-      };
-    };
+    dconf = import ./dconf.nix {gnomeExtensions = gnomeExtensions;};
   }
