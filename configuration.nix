49f2c4b13f27b123291e02bb5745524add41617f
Niclas Overby <niclas@overby.me>
2023-08-25 10:42:51 +0200

    services: add gnome-keyring

--- configuration.nix
+++ configuration.nix
@@ -96,6 +96,7 @@
       pulse.enable = true;
     };
     gnome = {
+      gnome-keyring.enable = true;
       gnome-browser-connector.enable = true;
     };
   };
--- home.nix
+++ home.nix
@@ -113,6 +113,10 @@ in
       vscodeExtensions = vscodeExtensions;
     };
 
+    services = {
+      gnome-keyring.enable = true;
+    };
+
     systemd = import ./systemd.nix {pkgs = pkgs;};
     dconf = import ./dconf.nix {gnomeExtensions = gnomeExtensions;};
   }
