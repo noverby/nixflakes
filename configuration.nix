559abbbd145f66dcba54e5506e38a7f09105393f
Niclas Overby <niclas@overby.me>
2023-10-08 11:26:08 +0200

    cleanup: use with pkgs

--- configuration.nix
+++ configuration.nix
@@ -97,23 +97,23 @@
   security.pam.services = {
     gdm.enableGnomeKeyring = true;
     login.fprintAuth = false;
-    gdm-fingerprint = {
+    gdm-fingerprint = with pkgs; {
       text = ''
         auth       required                    pam_shells.so
         auth       requisite                   pam_nologin.so
         auth       requisite                   pam_faillock.so      preauth
-        auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+        auth       required                    ${fprintd}/lib/security/pam_fprintd.so
         auth       optional                    pam_permit.so
         auth       required                    pam_env.so
-        auth       [success=ok default=1]      ${pkgs.gnome.gdm}/lib/security/pam_gdm.so
-        auth       optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+        auth       [success=ok default=1]      ${gnome.gdm}/lib/security/pam_gdm.so
+        auth       optional                    ${gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
 
         account    include                     login
 
         password   required                    pam_deny.so
 
         session    include                     login
-        session    optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+        session    optional                    ${gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
       '';
     };
   };
