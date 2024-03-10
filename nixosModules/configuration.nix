2b7a528f5aae44e27e36811347140b611f5c9cc8
Niclas Overby <niclas@overby.me>
2024-03-10 19:57:07 +0100

    udev: add xreal rules

--- nixosModules/configuration.nix
+++ nixosModules/configuration.nix
@@ -168,6 +168,25 @@
       xkbVariant = "";
       excludePackages = [pkgs.xterm];
     };
+    udev.extraRules = ''
+      # XReal
+
+      # Rule for USB devices
+      SUBSYSTEM=="usb", ACTION=="add", ATTR{idVendor}=="3318", ATTR{idProduct}=="0424|0428|0432", MODE="0666"
+
+      # Rule for Input devices (such as eventX)
+      SUBSYSTEM=="input", KERNEL=="event[0-9]*", ATTRS{idVendor}=="3318", ATTRS{idProduct}=="0424|0428|0432", MODE="0666"
+
+      # Rule for Sound devices (pcmCxDx and controlCx)
+      SUBSYSTEM=="sound", KERNEL=="pcmC[0-9]D[0-9]p", ATTRS{idVendor}=="3318", ATTRS{idProduct}=="0424|0428|0432", MODE="0666"
+      SUBSYSTEM=="sound", KERNEL=="controlC[0-9]", ATTRS{idVendor}=="3318", ATTRS{idProduct}=="0424|0428|0432", MODE="0666"
+
+      # Rule for HID Devices (hidraw)
+      SUBSYSTEM=="hidraw", KERNEL=="hidraw[0-9]*", ATTRS{idVendor}=="3318", ATTRS{idProduct}=="0424|0428|0432", MODE="0666"
+
+      # Rule for HID Devices (hiddev)
+      KERNEL=="hiddev[0-9]*", SUBSYSTEM=="usb", ATTRS{idVendor}=="3318", ATTRS{idProduct}=="0424|0428|0432", MODE="0666"
+    '';
   };
 
   # Systemd
