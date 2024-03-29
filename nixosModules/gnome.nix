62ece2cbccbff042c06c3f4b12f09d8d5bdfe0ea
Niclas Overby <niclas@overby.me>
2024-03-29 10:35:33 +0100

    pkgs: remove epiphany

--- nixosModules/gnome.nix
+++ nixosModules/gnome.nix
@@ -12,6 +12,7 @@
       desktopManager.gnome.enable = true;
     };
   };
+  environment.gnome.excludePackages = with pkgs; [epiphany];
   # Unmanaged gnome-extensions deps
   environment.sessionVariables = with pkgs; {
     GI_TYPELIB_PATH = map (pkg: "${pkg}/lib/girepository-1.0") [vte pango harfbuzz gtk3 gdk-pixbuf at-spi2-core];
