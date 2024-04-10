871f50a4c0a10a053166400b22b91e16ccf57d92
Niclas Overby <niclas@overby.me>
2024-04-10 17:39:19 +0200

    gravitas: move packages into module

--- nixosConfigurations/gravitas.nix
+++ nixosConfigurations/gravitas.nix
@@ -2,51 +2,21 @@
   inputs,
   src,
   ...
-}: let
-  inherit (inputs) nixpkgs roc stardustxr flatland;
-  system = "x86_64-linux";
-  pkgs = import nixpkgs {
-    inherit system;
-    config = {
-      allowUnfree = true;
-      packageOverrides = pkgs: {
-        roc = roc.packages.${system}.default;
-        stardustxr = stardustxr.packages.${system}.default;
-        flatland = flatland.packages.${system}.default;
-        monado = pkgs.monado.overrideAttrs (old: {
-          version = "unstable-2024-03-07";
-          src = pkgs.fetchFromGitHub {
-            owner = "noverby";
-            repo = "monado-mirror";
-            rev = "4bd442913eff792c4120b9195be6206a2d7a11cd";
-            hash = "sha256-sDGuNT2rmE25aRj3fP5hHUN76fUVqhLNebdv112cqpw=";
-          };
-        });
-        fprintd = pkgs.fprintd.overrideAttrs (_: {
-          mesonCheckFlags = [
-            "--no-suite"
-            "fprintd:TestPamFprintd"
-          ];
-        });
-      };
-    };
+}: {
+  specialArgs = {
+    inherit src inputs;
+    stateVersion = "24.05";
   };
-  stateVersion = "24.05";
-in
-  inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    specialArgs = {
-      inherit src pkgs inputs stateVersion;
-    };
-    modules = with inputs; [
-      self.nixosModules.framework
-      self.nixosModules.configuration
-      self.nixosModules.home-manager
-      self.nixosModules.gnome
-      nixos-cosmic.nixosModules.default
-      nixos-hardware
-      .nixosModules
-      .framework-13th-gen-intel
-      home-manager.nixosModules.home-manager
-    ];
-  }
+  modules = with inputs; [
+    nixos-hardware
+    .nixosModules
+    .framework-13th-gen-intel
+    self.nixosModules.framework
+    self.nixosModules.packages
+    self.nixosModules.configuration
+    self.nixosModules.home-manager
+    self.nixosModules.gnome
+    nixos-cosmic.nixosModules.default
+    home-manager.nixosModules.home-manager
+  ];
+}
---
+++ nixosModules/packages.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  nixpkgs.config = {
+    allowUnfree = true;
+    packageOverrides = with inputs;
+      prev: {
+        roc = roc.packages.${pkgs.system}.default;
+        stardustxr = stardustxr.packages.${pkgs.system}.default;
+        flatland = flatland.packages.${pkgs.system}.default;
+        monado = prev.monado.overrideAttrs (_: {
+          version = "unstable-2024-03-07";
+          src = prev.fetchFromGitHub {
+            owner = "noverby";
+            repo = "monado-mirror";
+            rev = "4bd442913eff792c4120b9195be6206a2d7a11cd";
+            hash = "sha256-sDGuNT2rmE25aRj3fP5hHUN76fUVqhLNebdv112cqpw=";
+          };
+        });
+        fprintd = prev.fprintd.overrideAttrs (_: {
+          mesonCheckFlags = [
+            "--no-suite"
+            "fprintd:TestPamFprintd"
+          ];
+        });
+      };
+  };
+}
