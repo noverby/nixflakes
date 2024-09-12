7e4dc4d8fafd628a125b246743f4865a1cd10da8
Niclas Overby <niclas@overby.me>
2024-09-12 10:56:35 +0200

    systemd: use sd-switch

--- home-modules/systemd.nix
+++ home-modules/systemd.nix
@@ -3,32 +3,35 @@
   homeDirectory,
   ...
 }: {
-  systemd.user.services = {
-    protonmail-bridge = {
-      Unit = {
-        Description = "Service to run the Protonmail bridge client";
-        After = "network.target";
+  systemd.user = {
+    startServices = "sd-switch";
+    services = {
+      protonmail-bridge = {
+        Unit = {
+          Description = "Service to run the Protonmail bridge client";
+          After = "network.target";
+        };
+        Service = {
+          ExecStart = "${pkgs.protonmail-bridge}/bin/protonmail-bridge --noninteractive";
+        };
+        Install = {
+          WantedBy = ["default.target"];
+        };
       };
-      Service = {
-        ExecStart = "${pkgs.protonmail-bridge}/bin/protonmail-bridge --noninteractive";
-      };
-      Install = {
-        WantedBy = ["default.target"];
-      };
-    };
-    xreal-air-driver = {
-      Unit = {
-        Description = "XREAL Air user-space driver";
-        After = "network.target";
-      };
-      Service = {
-        Type = "simple";
-        Environment = "HOME=${homeDirectory}";
-        ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.xr-linux-driver}/bin/xrealAirLinuxDriver'";
-        Restart = "on-failure";
-      };
-      Install = {
-        WantedBy = ["multi-user.target"];
+      xreal-air-driver = {
+        Unit = {
+          Description = "XREAL Air user-space driver";
+          After = "network.target";
+        };
+        Service = {
+          Type = "simple";
+          Environment = "HOME=${homeDirectory}";
+          ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.xr-linux-driver}/bin/xrealAirLinuxDriver'";
+          Restart = "on-failure";
+        };
+        Install = {
+          WantedBy = ["multi-user.target"];
+        };
       };
     };
   };
